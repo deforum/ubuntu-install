d330bcba3828f85d0aea48e3e112ae56f5ee941f
Brian Gilbert <brian@briangilbert.net>
2024-03-05 17:49:14 +1100

    Add docker and NVIDIA Container Toolkit

--- install_drivers.sh
+++ install_drivers.sh
@@ -85,3 +85,47 @@ conda config --set auto_activate_base false
 
 # Clean mambaforge install script
 rm ./Mambaforge-Linux-x86_64.sh
+
+# Install Docker https://docs.docker.com/engine/install/ubuntu/
+# Uninstall old versions
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Actually install docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Docker post install steps https://docs.docker.com/engine/install/linux-postinstall/
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo systemctl restart docker
+
+# Install NVIDIA Container Toolkit
+wget https://nvidia.github.io/libnvidia-container/gpgkey
+sudo gpg --dearmor ./gpgkey > ./temp-keyring.gpg
+sudo mv ./gpgkey.gpg /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+sudo wget https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list -P /etc/apt/sources.list.d/
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-ctk runtime configure --runtime=docker
+
+# Clean thios steps downloaded files
+sudo rm gpgkey temp-keyring.gpg
+
+#Echo relevant info at end
+
+echo "Important info"
